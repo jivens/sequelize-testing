1564987e892c8d7a839a9f6e7dfbb248ad147fc9
amyfou <36521868+amyfou@users.noreply.github.com>
2019-05-15 11:16:46 -0700

    poo emoji

--- index.js
+++ index.js
@@ -42,8 +42,13 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    operatorsAliases: false,
-    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+    //operatorsAliases: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    define: {
+		charset: 'utfmb4',
+		collate: 'utf8mb4_unicode_ci',
+		timestamps: true
+    }
   });
 
 sequelize
@@ -55,6 +60,32 @@ sequelize
   console.error('Unable to connect to the database:', err);
 });
 // ****** Set up default MYSQL connection END ****** //
+//
+// User model for sequelize, controlling connections to the application; not controlling connections to the db
+//
+const User = sequelize.define('user', {
+	name: { type: Sequelize.STRING },
+	email: { type: Sequelize.STRING },
+	password: { type: Sequelize.STRING }, 
+	roles: { type: Sequelize.STRING },
+},{
+	charset: 'utf8mb4',
+	collate: 'utf8mb4_general_ci'
+});
+
+// force creation of a table with the right schema, add the user
+User.sync({force: true}).then(() => {
+	// Table created
+	return User.create({
+		name: "Amy ɛ́ Fountain ʕ'ʷ 💩",
+		email: "a.v.fountain@gmail.com",
+		password: "a.v.fountain@gmail.com",
+		roles: "admin"
+	});
+})
+.then((user) => {
+	console.log(user);
+});
 
 app.use('/', (req, res) => res.send("Welcome COLRC User"));
 app.listen(process.env.GRAPHQLPORT, () => console.log('COLRC Enterprise Server is ready on localhost:' + process.env.GRAPHQLPORT));
