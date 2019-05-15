0930df3598e90ed0d0dea8e540c9464b1a9abf20
John Ivens <john.wagner.ivens@gmail.com>
2019-05-15 11:14:05 -0700

    Testing server round trip communication

--- index.js
+++ index.js
@@ -42,8 +42,14 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    operatorsAliases: false,
-    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+    //operatorsAliases: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_unicode_ci',
+      timestamps: true
+    },
+    //logging:false
   });
 
 sequelize
@@ -55,6 +61,49 @@ sequelize
   console.error('Unable to connect to the database:', err);
 });
 // ****** Set up default MYSQL connection END ****** //
+
+//
+// User model for sequelize
+//
+const User = sequelize.define('user', {
+  name: { type: Sequelize.STRING },
+  email: { type: Sequelize.STRING },
+  password: { type: Sequelize.STRING },
+  roles: { type: Sequelize.STRING }
+},
+{
+  charset: 'utf8mb4',
+  collate: 'utf8mb4_unicode_ci'
+});
+
+// force: true will drop the table if it already exists
+User
+.sync({force: true})
+.then(() => {
+  // Table created
+  return User.create({
+    name: "John Ivensɛ́ʕ'ʷ",
+    email: 'john.wagner.ivens@gmail.com',
+    password: 'john.wagner.ivens@gmail.com',
+    roles: "admin"
+  });
+})
+.then((user) => {
+  console.log(user);
+  return User.findOne({
+    where: { id: 1 }
+  }).then((res) => {
+    return [{
+      id: res.dataValues.id,
+      name: res.dataValues.name,
+      email: res.dataValues.email,
+      roles: res.dataValues.roles.split(',')
+    }];
+  });
+})
+.then((newuser) => {
+  console.log(newuser);
+});
 
 app.use('/', (req, res) => res.send("Welcome COLRC User"));
 app.listen(process.env.GRAPHQLPORT, () => console.log('COLRC Enterprise Server is ready on localhost:' + process.env.GRAPHQLPORT));
